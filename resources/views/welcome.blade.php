5fe6fbce6420542dd3e1ca571239472f84e77528
Chadoboy <nazar-90@ukr.net>
2020-06-10 11:10:41 +0300

    add welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Laravel !!!
+                    Laravel Project Congrats Welcome !!!
                 </div>
 
                 <div class="links">
